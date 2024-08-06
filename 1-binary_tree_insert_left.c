604a96bb9ab519f07b158e4226f81f68552eac11
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-06 21:58:48 +0000

    make binary tree insert left method

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Inserts a node as the left-child of another node
+ * @parent: Pointer to the node to insert child in
+ * @value: Value to store
+ *
+ * Return: Pointer to the new node or NULL on fail
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	binary_tree_t *new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+
+	return (new_node);
+}
