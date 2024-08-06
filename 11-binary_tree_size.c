4aa202bff9235930642293a3a977335326640d6b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-06 23:28:59 +0000

    make binary tree size method

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measure size of a binary tree
+ * @tree: Pointer to root node of tree to measure
+ *
+ * Return: Size of the tree or 0 if NULL
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (NULL);
+	}
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
