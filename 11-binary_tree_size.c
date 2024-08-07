3a915c228b27ffe17965b8e3a76a2a5469188029
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-07 21:55:45 +0000

    returning null on non null

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -4,14 +4,14 @@
  * binary_tree_size - Measure size of a binary tree
  * @tree: Pointer to root node of tree to measure
  *
- * Return: Size of the tree or 0 if NULL
+ * Return: Size of the tree or 0 if not
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
 
 	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
