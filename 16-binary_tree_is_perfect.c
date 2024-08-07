1bf7a9c6accc4502b21d86ffbd586c3393b297a6
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-07 22:13:31 +0000

    compilation errors, mixed declaration, pointer int comparison

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,10 +8,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0, right_height = 0;
+
 	if (tree == NULL)
 		return (0);
-
-	size_t left_height = 0, right_height = 0;
 
 	if (tree->left != NULL)
 		left_height = 1 + binary_tree_height(tree->left);
@@ -22,45 +22,29 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_perfect - Checks if tree is perfect
- * @tree: Pointer to root node of tree
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
  *
- * Return: 1 if perfect or 0 if not
+ * Return: 1 if the tree is perfect, otherwise 0
  */
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	signed right_height = 0;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
 
-	if (tree->left != right_height)
-	{
-		left_height = binary_tree_height(tree->left);
-	}
-	if (tree->right != NULL)
-	{
-		right_height = binary_tree_height(tree->right);
-	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height != right_height)
-	{
 		return (0);
-	}
 
 	if (tree->left == NULL && tree->right == NULL)
-	{
 		return (1);
-	}
 
 	if (tree->left != NULL && tree->right != NULL)
-	{
 		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
-	}
 
 	return (0);
 }
