f7aefd5ac9863a332b2b28a66a9627235da81dd6
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-07 21:57:25 +0000

    no mixed declarations

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -30,13 +30,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	signed right_height = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	size_t left_height = 0;
-	signed right_height = 0;
 
 	if (tree->left != right_height)
 	{
